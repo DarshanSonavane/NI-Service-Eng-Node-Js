848cc01121db326f019fcb2e7a14437bc519ff73
DarshanSonavane <darshansonavane24@gmail.com>
2024-05-17 01:35:53 +0530

    Removed hardcoded email added for testing

--- api/service/Mailer.js
+++ api/service/Mailer.js
@@ -11,7 +11,7 @@ let transporter = nodemailer.createTransport({
 });
 
 const sendMail = ( customerName,customercode, complaintType , machineType , employeeEmail , customerCity , customerMobile , requestType )=>{
-    let mailList = ['darshansonavane24@gmail.com'];//['nilesh@niserviceeng.com','complaints@niserviceeng.com'];
+    let mailList = ['nilesh@niserviceeng.com','complaints@niserviceeng.com'];
     let subject = "New Service Request"
     if(employeeEmail){
         subject += "Assigned To You"
