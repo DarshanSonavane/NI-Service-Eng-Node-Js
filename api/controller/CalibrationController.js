dfacdf08e8e79c471993cbcf50765d235ef92149
DarshanSonavane <darshansonavane24@gmail.com>
2024-06-26 00:30:17 +0530

    Minor changes

--- api/controller/CalibrationController.js
+++ api/controller/CalibrationController.js
@@ -217,16 +217,12 @@ const generateAndSendCalibration = async(req,res)=>{
                 const outputPath = `./assets/uploads/${calibrationrequestData['customerId']['customerName']}.pdf`;
                 // const options = { type: "A4" };
                 // const options = { type: 'A4'};
-                const options = {
-                    format: 'Letter',  // Try different formats like 'A4', 'Legal', etc.
-                    border: {
-                      top: '0.5in',    // Adjust margins as needed
-                      right: '0.5in',
-                      bottom: '0.5in',
-                      left: '0.5in'
-                    },
-                    zoom: 1.5
-                  };
+                var options = {
+                    format: 'A4',
+                    border: '1cm',
+                    zoomFactor: '1.0',
+                    // other options
+                };
 
                 try {
                     // Generate the PDF
