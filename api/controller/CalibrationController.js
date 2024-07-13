b46e08bdd2b05446d46893d73b6da8ec0dd72824
DarshanSonavane <darshansonavane24@gmail.com>
2024-07-13 21:54:52 +0530

    Added new machine details

--- api/controller/CalibrationController.js
+++ api/controller/CalibrationController.js
@@ -311,6 +311,16 @@ const insertMachineModel = async(req,res)=>{
         },{
             "MODEL": "NPM NUVOI10",
             "MACHINE_NO": 85246
+        },{
+            "MODEL": "NPM MGA-1",
+            "MACHINE_NO": 8564
+        },
+        {
+            "MODEL": "NPM SM 111B",
+            "MACHINE_NO": 7894
+        },{
+            "MODEL": "NPM NUVOI10",
+            "MACHINE_NO": 4568
         }];
         await MachineModel.insertMany(dbData).then((data)=>{
             return res.status(200).json({ code : "200" , message: "State List Created Successfully!!", data: data });
