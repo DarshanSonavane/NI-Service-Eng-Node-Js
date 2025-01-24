0f087d25aa74757ca23caa993698cf70a1d05541
DarshanSonavane <darshansonavane24@gmail.com>
2025-01-24 11:35:14 +0530

    Email footers added

--- api/controller/ServiceRequestController.js
+++ api/controller/ServiceRequestController.js
@@ -869,13 +869,12 @@ const genetrateAndSendAMCToCustomer = async(req,res)=>{
 
         <!-- Footer content with an embedded image -->
         <footer style="margin-top: 20px; font-size: 12px; color: green; text-align: left;">
-          <p>Best Regards</p>
-	  <img src="${constants.SERVER_FILE_PATH}NI-SERVICE-LOGO.jpg" alt="Company Logo" style="width: 100px; margin-top: 10px;" />
-          <p>NI SERVICE ENGINEERING PVT.LTD.</p>
-          <p>Office No.18,2nd Floor, GNP Gallaria  MIDC Road , Dombivali (E) 421202</p>
-          <p>Contact Us : 9892151843</p>
-          <p><a href="mailto:service@niserviceeng.com">service@niserviceeng.com</a></p>
-          <p><a href="http://www.niserviceeng.com" style="color: green;">Website</a></p>
+          <p><b>Best Regards</b></p>
+	      <img src="${constants.SERVER_FILE_PATH}NI-SERVICE-LOGO-GIF.gif" alt="Company Logo" style="width: 100px; margin-top: 10px;" />
+          <p><b>Office No.18,2nd Floor, GNP Gallaria  MIDC Road , Dombivali (E) 421202</b></p>
+          <p><b>Contact Us : 9892151843</b></p>
+          <p><b>Email : <a href="mailto:service@niserviceeng.com">service@niserviceeng.com</a></b></p>
+          <p><b><a href="http://www.niserviceeng.com" style="color: green;">Website</a></b></p>
           
         </footer>
       </body>
