8eb84a9869f25381cf52f8624d3ca96488631644
DarshanSonavane <darshansonavane24@gmail.com>
2025-01-24 23:06:53 +0530

    Mailer js changes

--- api/service/Mailer.js
+++ api/service/Mailer.js
@@ -6,12 +6,12 @@ let transporter = nodemailer.createTransport({
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-        user: 'admin@niserviceeng.com', // your domain email address
-        pass: 'admin@12345' // your password
+        user: 'service@niserviceeng.com', // your domain email address
+        pass: 'Service@12345' // your password
     },
     connectionTimeout: 10000,  // Set a higher connection timeout (milliseconds)
-    greetingTimeout: 5000, 
-});
+    greetingTimeout: 5000,
+}); 
 
 const sendMail = ( customerName,customercode, complaintType , machineType , employeeEmail , customerCity , customerMobile , requestType )=>{
   console.log('Employee Email------',employeeEmail);
