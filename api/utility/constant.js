003808529157f64e5cc69bb2d57a1ed798d5e488
DarshanSonavane <darshansonavane24@gmail.com>
2025-01-27 01:46:59 +0530

    changed server path

--- api/utility/constant.js
+++ api/utility/constant.js
@@ -1,6 +1,6 @@
 const NI_SERVICE_ENG_APP = "NI_SERVICE_ENG_APP"
 const LOCAL_FILE_PATH = "http://localhost:3000/"
-const SERVER_FILE_PATH = "http://16.170.250.91:3000/"
+const SERVER_FILE_PATH = "http://13.49.111.133:3000/"
 
 module.exports ={
     NI_SERVICE_ENG_APP : NI_SERVICE_ENG_APP,
